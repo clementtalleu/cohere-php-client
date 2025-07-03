603314da69cf9b2ce44c4f2562c5ec4fce76a888
clementtalleu <clement.talleu@gmail.com>
2025-07-03 23:33:15 +0200

    fix interface cohere client

--- src/Client/CohereClientInterface.php
+++ src/Client/CohereClientInterface.php
@@ -5,10 +5,49 @@ declare(strict_types=1);
 namespace Talleu\CohereClient\Client;
 
 use Talleu\CohereClient\Resources\Chat\Chat;
+use Talleu\CohereClient\Resources\Classify\Classify;
+use Talleu\CohereClient\Resources\Connector\Connector;
+use Talleu\CohereClient\Resources\Dataset\Dataset;
+use Talleu\CohereClient\Resources\Detokenize\Detokenize;
+use Talleu\CohereClient\Resources\Embed\Embed;
+use Talleu\CohereClient\Resources\EmbedJob\EmbedJob;
+use Talleu\CohereClient\Resources\FineTuning\FineTuning;
+use Talleu\CohereClient\Resources\Model\Model;
+use Talleu\CohereClient\Resources\Rerank\Rerank;
+use Talleu\CohereClient\Resources\Tokenize\Tokenize;
 
 interface CohereClientInterface
 {
     public function chat(): Chat;
 
+    public function embed(): Embed;
+
+    public function rerank(): Rerank;
+
+    public function classify(): Classify;
+
+    public function fineTuning(): FineTuning;
+
+    public function dataset(): Dataset;
+
+    public function tokenize(): Tokenize;
+
+    public function detokenize(): Detokenize;
+
+    public function embedJob(): EmbedJob;
+
+    public function connector(): Connector;
+
+    public function model(): Model;
+
     public function sendRequest(string $method, string $path, array $body): array;
+
+    public function sendMultipartRequest(
+        string $method,
+        string $path,
+        array $formFields,
+        string $fileFieldName,
+        string $filePath,
+        ?string $fileMimeType = null
+    ): array;
 }
